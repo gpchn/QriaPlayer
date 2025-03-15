11fc63182304f99227ccd6e58ca73283197b2534
gpchn <gprogrammer@163.com>
2025-03-15 15:12:14 +0800

    [refactor] give up qq music

--- paio_download.py
+++ paio_download.py
@@ -8,35 +8,34 @@ from httpx import get
 
 init()
 
-config_path = Path("config.toml")
-config = loads_toml(config_path.read_text())
-NCM_DLINK_API = config.get("ncm").get("direct_link")
-NCM_SEARCH_API = config.get("ncm").get("search")
-NCM_LYRIC_API = config.get("ncm").get("lyric")
-QQ_DLINK_API = config.get("qq").get("direct_link")
-QQ_SEARCH_API = config.get("qq").get("search")
-KUGOU_DLINK_API = config.get("kugou").get("direct_link")
-KUGOU_SEARCH_API = config.get("kugou").get("search")
+CONFIG_PATH = Path("config.toml")
+CONFIG = loads_toml(CONFIG_PATH.read_text())
+# 网易云音乐
+NCM_SEARCH_API = CONFIG["api"]["ncm"]["search"]
+NCM_DLINK_API = CONFIG["api"]["ncm"]["direct_link"]
+NCM_LYRIC_API = CONFIG["api"]["ncm"]["lyric"]
+# 酷狗音乐
+KUGOU_SEARCH_API = CONFIG["api"]["kugou"]["search"]
+KUGOU_DLINK_API = CONFIG["api"]["kugou"]["direct_link"]
+KUGOU_LYRIC_API = CONFIG["api"]["kugou"]["lyric"]
 
 
 def main():
     while True:
         # 搜索歌曲
         name = input(f"请输入歌曲名：")
-        """
+        # 请求各平台搜索接口
         ncm_songs = ncm_search(name)
-        qq_songs  = qq_search(name)
-        songs = ncm_songs + qq_songs
-        """
-        songs = ncm_search(name)
+        kugou_songs = kugou_search(name)
+        songs = ncm_songs +  kugou_songs
 
         # 指定序号
-        choosed_inedx = input("请输入序号（1~10）：")
+        choosed_inedx = input("请输入序号（1~20）：")
         if not choosed_inedx.isdigit():
             input("输入有误，操作取消")
             exit(1)
         choosed_inedx = int(choosed_inedx)
-        if choosed_inedx > 10:
+        if choosed_inedx < 1 or choosed_inedx > 20:
             input("输入有误，操作取消")
             exit(1)
 
@@ -47,17 +46,12 @@ def main():
 
         # 解析直链
         print("正在下载歌曲……")
-        """
-        if choosed_inedx <= 5:
+        if choosed_inedx <= 10:
             mp3_data = ncm_analyze(music_id)
             lrc_data = ncm_lyric(music_id)
-        elif choosed_inedx <= 10:
-            mp3_data = qq_analyze(music_id)
-        elif choosed_inedx <= 15:
+        elif choosed_inedx <= 20:
             mp3_data = kugou_analyze(music_id)
-        """
-        mp3_data = ncm_analyze(music_id)
-        lrc_data = ncm_lyric(music_id)
+            lrc_data = kugou_lyric(music_id)
 
         # 创建文件的 Path 对象
         music_name = f"{choosed_song[1]} - {choosed_song[2]}"
@@ -72,6 +66,7 @@ def main():
         # 写入数据
         save_path.write_bytes(mp3_data)
         lrc_path.write_text(lrc_data, encoding="utf-8")
+        print("下载完成！")
 
 
 # 网易云音乐搜索
@@ -115,41 +110,42 @@ def ncm_lyric(id: int) -> str:
     return lrc
 
 
-# QQ 音乐搜索
-def qq_search(name: str, num: int = 5) -> list:
-    resq_search = get(QQ_SEARCH_API % (name, num))
-    search_result = loads_json(resq_search.text).get("data").get("song").get("list")
+# 酷狗音乐搜索
+def kugou_search(name: str, num: int = 10) -> list:
+    resq_search = get(KUGOU_SEARCH_API % (name, num))
+    search_result = loads_json(resq_search.text).get("data").get("lists")
     songs = []
 
-    for index, song in enumerate(search_result, 6):
-        formatted_data = song.get("f").split("|")
-        song_id = formatted_data[0]
-        song_name = formatted_data[1].split("(")[0].rstrip()
-        song_artist = formatted_data[3]
+    for index, song in enumerate(search_result, 11):
+        song_id = song.get("FileHash")
+        song_name = song.get("SongName")
+        song_artist = song.get("SingerName")
         # 一首歌提取成一个元组推到 songs 列表里
         songs.append((song_id, song_name, song_artist))
         # 输出信息供选择
-        print(f"[{index}] {Fore.GREEN}{song_name} - {song_artist}{Fore.RESET}")
+        print(f"[{index}] {Fore.BLUE}{song_name} - {song_artist}{Fore.RESET}")
 
     return songs
 
 
-# QQ 音乐直链解析
-def qq_analyze(id: int) -> bytes:
-    # 获取 id 对应的音乐直链
-    resq_analyze = get(QQ_DLINK_API % id)
-    dlink = loads_json(resq_analyze.text)[0]
-    input(dlink)
+# 酷狗音乐直链解析
+def kugou_analyze(music_id: str) -> bytes:
+    resq_analyze = get(KUGOU_DLINK_API % music_id)
+    input(resq_analyze.json())
+    input(resq_analyze.text)
+    dlink = loads_json(resq_analyze.text).get("url")
     # 下载 mp3 二进制数据
-    resq_mp3 = get(resq_analyze)
+    resq_mp3 = get(dlink)
 
     return resq_mp3.content
 
 
-def kugou_search(): ...
+# 酷狗音乐歌词解析
+def kugou_lyric(music_id: str) -> str:
+    resq_lrc = get(KUGOU_LYRIC_API % music_id)
+    lrc = loads_json(resq_lrc.text).get("data").get("lyrics")
 
-
-def kugou_analyze(): ...
+    return lrc
 
 
 if __name__ == "__main__":
